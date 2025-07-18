7e8ec78a8a370db00260112da8f4cdc398b55ca8
Alex-Iset <fl3shtrader@yandex.ru>
2025-07-18 19:08:50 +0300

    added: new app "labels"

--- task_manager/constants.py
+++ task_manager/constants.py
@@ -1,13 +1,13 @@
-FIELD_TEXTS = {
+USER_FIELD_TEXTS = {
     'first_name': {
         'label': 'Имя',
         'placeholder': 'Имя',
-        'help_text': 'Обязательное поле. Не более 150 символов.'
+        'help_text': ''
     },
     'last_name': {
         'label': 'Фамилия',
         'placeholder': 'Фамилия',
-        'help_text': 'Обязательное поле. Не более 150 символов.'
+        'help_text': ''
     },
     'username': {
         'label': 'Имя пользователя',
@@ -26,6 +26,29 @@ FIELD_TEXTS = {
     }
 }
 
+TASK_FIELD_TEXTS = {
+    'name': {
+        'label': 'Имя',
+        'placeholder': 'Имя'
+    },
+    'description': {
+        'label': 'Описание',
+        'placeholder': 'Описание'
+    },
+    'status': {
+        'label': 'Статус',
+        'placeholder': ''
+    },
+    'executor': {
+        'label': 'Исполнитель',
+        'placeholder': ''
+    },
+    'labels': {
+        'label': 'Метки',
+        'placeholder': ''
+    }
+}
+
 SUCCESS_MESSAGES = {
     'user': {
         'user_created': 'Пользователь успешно зарегистрирован',
@@ -38,10 +61,14 @@ SUCCESS_MESSAGES = {
         'status_deleted': 'Статус успешно удален',
     },
     'task':{
-
+        'task_created': 'Задача успешно создана',
+        'task_updated': 'Задача успешно изменена',
+        'task_deleted': 'Задача успешно удалена',
     },
     'label': {
-
+        'label_created': 'Метка успешно создана',
+        'label_updated': 'Метка успешно изменена',
+        'label_deleted': 'Метка успешно удалена',
     },
     'logged_in': 'Вы залогинены',
     'logged_out': 'Вы разлогинены',
@@ -52,8 +79,10 @@ ERROR_MESSAGES = {
     'no_permission': 'У вас нет прав для этого действия.',
     'no_permission_update': 'У вас нет прав для изменения другого пользователя.',
     # 'no_permission_delete': 'У вас нет прав для удаления другого пользователя.', - не используется в демонстрационном проекте
-    'user_has_tasks': 'Невозможно удалить пользователя, потому что он используется.',
+    'user_has_tasks': 'Невозможно удалить пользователя, потому что он используется',
     'status_using': 'Невозможно удалить статус, потому что он используется',
+    'only_author_can_delete': 'Задачу может удалить только ее автор',
+    'label_using': 'Невозможно удалить метку, потому что она используется',
     'login_invalid': (
         "Пожалуйста, введите правильные имя пользователя и пароль. "
         "Оба поля могут быть чувствительны к регистру."
---
+++ task_manager/labels/views.py
@@ -0,0 +1,60 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.messages.views import SuccessMessageMixin
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+from django.shortcuts import redirect
+from django.contrib import messages
+
+from task_manager.labels.models import Label
+from task_manager.labels.forms import LabelForm
+from task_manager.constants import SUCCESS_MESSAGES, ERROR_MESSAGES
+
+
+class LabelListView(LoginRequiredMixin, ListView):
+    model = Label
+    template_name = 'labels/list.html'
+    context_object_name = 'labels'
+
+
+class LabelCreateView(LoginRequiredMixin, SuccessMessageMixin, CreateView):
+    model = Label
+    form_class = LabelForm
+    template_name = 'labels/form.html'
+    success_url = reverse_lazy('labels_list')
+    success_message = SUCCESS_MESSAGES['label']['label_created']
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['form_title'] = 'Создать метку'
+        context['submit_button_text'] = 'Создать'
+        return context
+
+
+class LabelUpdateView(LoginRequiredMixin, SuccessMessageMixin, UpdateView):
+    model = Label
+    form_class = LabelForm
+    template_name = 'labels/form.html'
+    success_url = reverse_lazy('labels_list')
+    success_message = SUCCESS_MESSAGES['label']['label_updated']
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['form_title'] = 'Изменение метки'
+        context['submit_button_text'] = 'Изменить'
+        return context
+
+
+class LabelDeleteView(LoginRequiredMixin, SuccessMessageMixin, DeleteView):
+    model = Label
+    template_name = 'labels/delete.html'
+    success_url = reverse_lazy('labels_list')
+    success_message = SUCCESS_MESSAGES['label']['label_deleted']
+    error_message = ERROR_MESSAGES['label_using']
+
+    def form_valid(self, form):
+        status = self.get_object()
+        if status.tasks.exists():
+            messages.error(self.request, self.error_message)
+            return redirect(self.success_url)
+        return super().form_valid(form)
+
