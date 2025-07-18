08ccec0bc0e9ef5080a4a2964ef67d71684f67ca
Alex-Iset <fl3shtrader@yandex.ru>
2025-07-19 01:06:45 +0300

    testing: rollbar

--- task_manager/views.py
+++ task_manager/views.py
@@ -3,3 +3,12 @@ from django.views.generic.base import TemplateView
 
 class HomePageView(TemplateView):
     template_name = 'homepage.html'
+
+    def get(self, request, *args, **kwargs):
+        # Для Rollbar
+        try:
+            a.hello()
+        except Exception as e:
+            import rollbar
+            rollbar.report_exc_info(request=request)
+        return super().get(request, *args, **kwargs)
