416f162ec5ffdfc333e103918ada46783ff624c0
Alex-Iset <fl3shtrader@yandex.ru>
2025-07-20 09:18:21 +0300

    added: administration setup is completed

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -151,7 +151,7 @@ AUTH_USER_MODEL = 'users.User'
 # Internationalization
 # https://docs.djangoproject.com/en/5.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru-RU'
 
 TIME_ZONE = 'UTC'
 
--- task_manager/urls.py
+++ task_manager/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 from task_manager.views import HomePageView
 from task_manager.users.views import UserLoginView, UserLogoutView
@@ -31,3 +33,6 @@ urlpatterns = [
     path('logout/', UserLogoutView.as_view(), name='logout'),
     path('admin/', admin.site.urls),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
